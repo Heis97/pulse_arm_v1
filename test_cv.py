4345d7ab6532b395a459515ea1e8c0b18fbb6632
Heis97 <46742591+Heis97@users.noreply.github.com>
2024-06-25 10:46:18 +0300

    2506

--- test_cv.py
+++ test_cv.py
@@ -6,19 +6,25 @@ if not cap.isOpened():
  exit()
 cap.set(cv.CAP_PROP_FRAME_WIDTH,1280)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT,720)
+frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+frame_size = (frame_width,frame_height)
+output = cv.VideoWriter('output_video_from_file.mp4', cv.VideoWriter_fourcc(*'h264'), 20, frame_size)
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
     # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+    if ret:
+        output.write(frame)
+        cv.imshow('frame', frame)
         # Our operations on the frame come here
     #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # Display the resulting frame
-    cv.imshow('frame', frame)
+    
     if cv.waitKey(1) == ord('q'):
+        
         break
 # When everything done, release the capture
+output.release()
 cap.release()
 cv.destroyAllWindows()
